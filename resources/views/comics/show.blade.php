47ecd5c5686f2690ecb1b96f80f5941d211c6272
Antonio Stassi <stassiant@outlook.it>
2024-10-22 15:50:23 +0200

    Correcting show.blade layout columns for artists/writers informations

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -5,25 +5,30 @@
     <h1>comics.show blade id: {{$comic->id}}</h1>
     <div class="card text-center p-4">
         <div class="row">
-            
             <div class="col-6">
                 <h1 class="text-danger">{{$comic->title}}</h1>
                 <p><strong>Description: <br></strong> {{$comic->description}}</p>
                 <p><strong>Series: <br></strong> {{$comic->series}}</p>
                 <p><strong>Price: <br></strong> {{$comic->price}}</p>
                 <p><strong>Type: <br></strong> {{$comic->type}}</p>
-                <div class="artists border p-3">
+            </div>
+            <div class="col-6 mb-2">
+                <img src="{{$comic->thumb}}" alt="{{$comic->title}}">
+            </div>
+        </div>
+
+        <div class="row border p-2">
+            <div class="col-6">
+                <div class="artists p-3">
                     <h5>Artists:</h5>
                     <?php $artistsList = json_decode($comic->artists); ?>
                     @foreach ($artistsList as $artist)
                         <p>{{ $artist }}</p>
                     @endforeach
                 </div>
-                
             </div>
             <div class="col-6">
-                <img src="{{$comic->thumb}}" alt="{{$comic->title}}">
-                <div class="writers mt-3 border p-3">
+                <div class="writers p-3">
                     <h5>Writers:</h5>
                     <?php $writersList = json_decode($comic->writers); ?>
                     @foreach ($writersList as $writer)
@@ -56,7 +61,7 @@
         }
 
         img{
-            max-width:300px;
+            max-width:320px;
             text-align:center;
         }
 
