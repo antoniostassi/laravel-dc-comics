fab197c58b0031e73d964215d92139dc427662f7
Antonio Stassi <stassiant@outlook.it>
2024-10-22 14:39:26 +0200

    comics table migration

---
+++ database/migrations/2024_10_22_122856_create_comics.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 64);
+            $table->string('description', 1024);
+            $table->smallInteger('price')->signed();
+            $table->string('series', 64);
+            $table->date('sale_date');
+            $table->string('type', 30);
+            $table->json('artists');
+            $table->json('writers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
