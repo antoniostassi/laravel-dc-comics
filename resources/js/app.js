36911487037479ae8016a41424e585b924a3fe7e
Antonio Stassi <stassiant@outlook.it>
2024-10-22 16:39:56 +0200

    Author and Writers plus button inputs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,23 @@
 import './bootstrap';
 import 'bootstrap'; // Framework SCSS Bootstrap 5.3.3
+
+
+let author = document.getElementById('author');
+let buttonAuthor = document.getElementById('add-author-input');
+let authorNumber = 1; 
+
+buttonAuthor.addEventListener('click', function(e) {
+    e.preventDefault();
+    authorNumber ++; 
+    author.innerHTML += `<input type="text" class="form-control mt-1" id="author-${authorNumber}" name="author" maxlength="30" placeholder="Artista n° ${authorNumber}">`
+});
+
+let writers = document.getElementById('writers');
+let buttonWriters = document.getElementById('add-writers-input');
+let writersNumber = 1; 
+
+buttonWriters.addEventListener('click', function(e) {
+    e.preventDefault();
+    writersNumber ++; 
+    writers.innerHTML += `<input type="text" class="form-control mt-1" id="writers-${writersNumber}" name="writers" maxlength="30" placeholder="Scrittore n° ${writersNumber}">`
+});
