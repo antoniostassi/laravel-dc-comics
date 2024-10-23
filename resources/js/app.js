464537fe0db117d87c5211a21bddf66a04d9c2c1
Antonio Stassi <stassiant@outlook.it>
2024-10-23 13:52:53 +0200

    removed useless stuffs on input writers/artists creation

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,20 +4,16 @@ import 'bootstrap'; // Framework SCSS Bootstrap 5.3.3
 
 let author = document.getElementById('author');
 let buttonAuthor = document.getElementById('add-author-input');
-let authorNumber = 1; 
 
 buttonAuthor.addEventListener('click', function(e) {
     e.preventDefault();
-    authorNumber ++; 
-    author.innerHTML += `<input type="text" class="form-control mt-1" name="author[]" maxlength="30" placeholder="Artista n° ${authorNumber}">`
+    author.innerHTML += `<input type="text" class="form-control mt-1" name="author[]" maxlength="30" placeholder="Inserisci l'artista...">`
 });
 
 let writers = document.getElementById('writers');
 let buttonWriters = document.getElementById('add-writers-input');
-let writersNumber = 1; 
 
 buttonWriters.addEventListener('click', function(e) {
     e.preventDefault();
-    writersNumber ++; 
-    writers.innerHTML += `<input type="text" class="form-control mt-1" name="writer[]" maxlength="30" placeholder="Scrittore n° ${writersNumber}">`
+    writers.innerHTML += `<input type="text" class="form-control mt-1" name="writer[]" maxlength="30" placeholder="Inserisci lo scrittore...">`
 });
